f64ec64fc17601cf623df651e78a2b094f716dac
sharath1608 <sharath1608@gmail.com>
2026-03-25 08:56:34 -0700

    main.c - Fix imbalanced row distribution of the Mandelbrot function

--- main.c
+++ main.c
@@ -90,7 +90,7 @@ void monte_carlo_pi(long num_samples) {
 typedef struct {
     int *iters;
     int width, height, max_iter;
-    int row_start, row_end;
+    int thread_id, stride;
     double x_min, x_max, y_min, y_max;
     long inside;
 } MandelChunk;
@@ -98,7 +98,7 @@ typedef struct {
 static void mandel_worker(void *arg) {
     MandelChunk *c = (MandelChunk *)arg;
     long inside = 0;
-    for (int py = c->row_start; py < c->row_end; py++) {
+    for (int py = c->thread_id; py < c->height; py += c->stride) {
         for (int px = 0; px < c->width; px++) {
             double cr = c->x_min + (c->x_max - c->x_min) * px / c->width;
             double ci = c->y_min + (c->y_max - c->y_min) * py / c->height;
@@ -127,7 +127,6 @@ void mandelbrot(int width, int height, int max_iter) {
     double y_min = -1.5, y_max = 1.5;
 
     MandelChunk *chunks = (MandelChunk *)malloc(num_threads * sizeof(MandelChunk));
-    int rows_per = height / num_threads;
 
     for (int i = 0; i < num_threads; i++) {
         chunks[i].iters = iters;
@@ -136,8 +135,8 @@ void mandelbrot(int width, int height, int max_iter) {
         chunks[i].max_iter = max_iter;
         chunks[i].x_min = x_min; chunks[i].x_max = x_max;
         chunks[i].y_min = y_min; chunks[i].y_max = y_max;
-        chunks[i].row_start = i * rows_per;
-        chunks[i].row_end = (i == num_threads - 1) ? height : (i + 1) * rows_per;
+        chunks[i].thread_id = i;
+        chunks[i].stride = num_threads;
         chunks[i].inside = 0;
     }
 
@@ -476,8 +475,8 @@ void brute_force_crack(int password_len) {
 
 int main(int argc, char *argv[]) {
 
-    if (argc < 6) {
-        printf("Usage: main_original <num_samples> <max_iter> <num_intervals> <rt_size> <pw_len> <num_threads>\n");
+    if (argc < 7) {
+        printf("Usage: main_original <num_samples> <mandel_size> <max_iter> <num_intervals> <rt_size> <pw_len> <num_threads>\n");
         return 1;
     }
 
@@ -486,14 +485,14 @@ int main(int argc, char *argv[]) {
     init_global_threadpool();
 
     long num_samples    = atol(argv[1]);
-//    int mandel_size     = atoi(argv[2]);
-    int max_iter        = atoi(argv[2]);
-    long num_intervals  = atol(argv[3]);
-    int rt_size         = atoi(argv[4]);
-    int pw_len          = atoi(argv[5]);
+    int mandel_size     = atoi(argv[2]);
+    int max_iter        = atoi(argv[3]);
+    long num_intervals  = atol(argv[4]);
+    int rt_size         = atoi(argv[5]);
+    int pw_len          = atoi(argv[6]);
 
     monte_carlo_pi(num_samples);
-//    mandelbrot(mandel_size, mandel_size, max_iter);
+    mandelbrot(mandel_size, mandel_size, max_iter);
     numerical_integration(num_intervals, 0.0, 10.0);
     ray_trace(rt_size, rt_size);
     brute_force_crack(pw_len);
